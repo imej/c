1267baf83052b8ea8e3ce0c0206337e14a0b3458
Richard Cao <cao_xuelei@hotmail.com>
2015-09-02 15:59:03 -0600

    20150902 dynamic lib test

---
+++ mylib/tests/uc_tests.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "dbg.h"
+#include <dlfcn.h>
+
+typedef int (*lib_function)(const char *data);
+
+int main()
+{
+    int rc = 0;
+
+    char *lib_file = "build/libex29.so";
+    char *func_to_run = "uppercase";
+    char *data = "Hello my friend";
+
+    void *lib = dlopen(lib_file, RTLD_NOW);
+    check(lib != NULL, "Failed to open the library %s: %s", lib_file, dlerror());
+
+    lib_function func = dlsym(lib, func_to_run);
+    check(func != NULL, "Did not find %s function in the library %s: %s", func_to_run, lib_file, dlerror());
+
+    rc = func(data);
+    check(rc == 0, "Function %s return %d for data: %s", func_to_run, rc, data);
+
+    rc = dlclose(lib);
+    check(rc == 0, "Failed to close %s", lib_file);
+
+    return 0;
+
+error: 
+    return 1;
+}
+
+
