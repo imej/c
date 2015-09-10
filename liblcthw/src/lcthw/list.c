eb8487183e7b925e122f008c0d0aded2fec8cbc4
Richard Cao <cao_xuelei@hotmail.com>
2015-09-10 16:10:09 -0600

    20150910 - complete my list

--- liblcthw/src/lcthw/list.c
+++ liblcthw/src/lcthw/list.c
@@ -48,9 +48,8 @@ void *List_pop(List *list)
 
     void *value;
 
-    if (list->count > 0) {
-        value = list->last->value;
-    } else {
+    if (!(list->count > 0)) {
+        // empty list, return NULL
         return NULL;
     }
 
@@ -66,6 +65,7 @@ void *List_pop(List *list)
         list->last = newLast;
     }
     list->count -= 1;
+    value = lastNode->value;
     free(lastNode);
 
     return value;
@@ -77,24 +77,115 @@ void List_clear(List *list)
 
     assert(list != NULL);
     while (list->first != NULL) {
-        tmpLN = list->first;
-	list->first = tmpLN.next;
+        // disconnect the first element
+	tmpLN = list->first;
+	list->first = tmpLN->next;
 	tmpLN->next = NULL;
-	list->first->prev = NULL;
+	if (list->first != NULL) {
+	    list->first->prev = NULL;
+	}
+        
+	// distroy the old first element
+	free(tmpLN);
+	list->count -= 1;
     }
+}
+
+void List_destroy(List *list) 
+{
+    assert(list != NULL);
+
+    if (list->count > 0) {
+        // not empty
+	printf("List_destroy: We only can destroy an empty list.");
+    } else {
+        free(list);
+    }
+}
+
+void List_clear_destroy(List *list) 
+{
+    List_clear(list);
+    List_destroy(list);
+}
+
+void *List_remove(List *list, ListNode *node)
+{
+    assert(list != NULL);
+    assert(node != NULL);
+
+    struct ListNode *prevNode;
+    struct ListNode *nextNode;
+    void *value = NULL;
+
+    if (node->next == NULL) {
+        // last element of the list, pop
+	value = List_pop(list);
+    } else {
+        if (node->prev == NULL) {
+	    // first element
+            nextNode = node->next;
+	    
+	    list->first = nextNode;
+            nextNode->prev = NULL;
+	    node->next = NULL;
+	} else {
+	    // in the middle of the list
+            prevNode = node->prev;
+	    nextNode = node->next;
+
+	    prevNode->next = nextNode;
+	    nextNode->prev = prevNode;
+	    node->prev = NULL;
+	    node->next = NULL;
+	}
+	
+	value = node->value;
+	free(node);
+    }
+    return value;
 }
 
 int main() 
 {
     struct List *list = List_create();
-    char *myStr = "good";
-    
-    List_push(list, myStr);
+    struct ListNode *ln, *tmp;
 
-    printf("list->count = %d\n", list->count);
+    List_push(list, "Toronto");
+    List_push(list, "Edmonton");
+    List_push(list, "Vancouver");
+    List_push(list, "Calgory");
+    List_push(list, "St. Albert");
+    List_push(list, "Ottawa");
+    List_push(list, "Montreal");
+    List_push(list, "Quebec");
+    List_push(list, "Halifax");
+    
+    printf("list->count = %d\n", List_count(list));
+    printf("first item: %s\n", (char *)List_first(list));
+    printf("last item: %s\n", (char *)List_last(list));
+    
+    printf("\nStart each: -----------------\n");
+    LIST_FOREACH(list, first, next, tmp, ln)
+    printf("++>%s\n", (char *)ln->value);
+    printf("\nEnd each: -----------------\n");
 
     List_pop(list);
 
-    printf("list->count = %d\n", list->count);
+    printf("list->count = %d\n", List_count(list));
+
+    printf("\nStart each: -----------------\n");
+    LIST_FOREACH(list, first, next, tmp, ln)
+    printf("++>%s\n", (char *)ln->value);
+    printf("\nEnd each: -----------------\n");
+
+    printf("Clear & Destroy list.");
+    List_clear_destroy(list);
+
+    printf("\nStart each: -----------------\n");
+    LIST_FOREACH(list, first, next, tmp, ln)
+    printf("++>%s\n", (char *)ln->value);
+    printf("\nEnd each: -----------------\n");
+
     return 1;
 }
