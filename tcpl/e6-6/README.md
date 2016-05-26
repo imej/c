7b8b111dadcb95c307c33043a5345b29e0762132
Richard Cao <cao_xuelei@hotmail.com>
2016-05-26 16:42:05 -0600

    e6-6 init

---
+++ tcpl/e6-6/README.md
@@ -0,0 +1,3 @@
+This is for exercise 6-6. p145
+
+Implement a simple verion of the #define processor. (no arguments)
---
+++ tcpl/e6-6/src/read.h
@@ -0,0 +1,4 @@
+#define BUFSIZE 100
+
+int getch(void);   /* get a (possibly pushed back) character */
+void ungetch(int c);  /* push character back on input */
